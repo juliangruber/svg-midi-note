dad7f9f15290b6769100df5b180616659f82086e
Julian Gruber <julian@juliangruber.com>
2017-04-16 14:07:15 +0200

    add .x && .y, for caching

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ module.exports = () => component({
   name: 'svg-midi-note',
   pure: true
 }).on('render', function () {
-  const { height, width, velocity, selected, onclick } = this.props
+  const { height, width, velocity, selected, onclick, x: x=0, y: y=0 } = this.props
   const hue = 280 - velocity * 280
   const bg = selected ? `hsl(${hue}, 80%, 70%)` : `hsl(${hue}, 100%, 50%)`
   const border = selected ? `hsl(${hue}, 100%, 38%)` : bg
   const line = selected ? border : 'hsl(0, 100%, 97%)'
 
   return html`
-    <g onclick=${onclick || noop}>
+    <g onclick=${onclick || noop} transform="translate(${x, y})">
       <rect x="1" y="1" height="${height - 2}" width="${width - 2}" stroke-width="1" stroke="black">
       <rect fill="${bg}" x="3" y="3" height="${height - 6}" width="${width - 6}" stroke-width="2" stroke="${border}" />
       <line x1="10" y1="${height / 2 + 1}" x2="${(width - 15 - 2) * velocity + 15}" y2="${height / 2 + 1}" stroke-width="2" stroke="${line}" />
